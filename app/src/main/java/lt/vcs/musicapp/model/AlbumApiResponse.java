e36ba014fa4994684df998cd7fcfa42f3bc799c3
JulitaDargyte <julita.dargyte@bazaarvoice.com>
2022-01-30 17:49:42 +0200

    Add AlbumApiResponse.java

---
+++ app/src/main/java/lt/vcs/musicapp/model/AlbumApiResponse.java
@@ -0,0 +1,25 @@
+package lt.vcs.musicapp.model;
+
+public class AlbumApiResponse {
+
+    private Album album;
+
+    public AlbumApiResponse(Album album) {
+        this.album = album;
+    }
+
+    public Album getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(Album album) {
+        this.album = album;
+    }
+
+    @Override
+    public String toString() {
+        return "AlbumApiResponse{" +
+                "album=" + album +
+                '}';
+    }
+}
