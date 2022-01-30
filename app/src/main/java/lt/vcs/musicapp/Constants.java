3e7dc798562702200d61c49497c661ef7afe1ea8
JulitaDargyte <julita.dargyte@bazaarvoice.com>
2022-01-30 13:51:06 +0200

    Add Constants.java

---
+++ app/src/main/java/lt/vcs/musicapp/Constants.java
@@ -0,0 +1,8 @@
+package lt.vcs.musicapp;
+
+public class Constants {
+
+    public static final String API_KEY = "20f68a033cdc47a0c008325db5e54514";
+    public static final String LOG_TAG = "app_test";
+
+}
