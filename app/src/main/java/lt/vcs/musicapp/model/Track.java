cdf0417343f415b8f39109c3fcd47f98bfb20a54
JulitaDargyte <julita.dargyte@bazaarvoice.com>
2022-01-30 18:14:36 +0200

    Add Track.java

---
+++ app/src/main/java/lt/vcs/musicapp/model/Track.java
@@ -0,0 +1,25 @@
+package lt.vcs.musicapp.model;
+
+public class Track {
+
+    private String name;
+
+    public Track(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Track{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
