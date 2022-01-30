d07dbf2494d03aaf1e2791b9cd400c98c5df1930
JulitaDargyte <julita.dargyte@bazaarvoice.com>
2022-01-30 17:35:55 +0200

    Add getArtistSearchResults() method

--- app/src/main/java/lt/vcs/musicapp/network/UserDataService.java
+++ app/src/main/java/lt/vcs/musicapp/network/UserDataService.java
@@ -3,6 +3,7 @@ package lt.vcs.musicapp.network;
 import java.util.List;
 
 import lt.vcs.musicapp.model.ArtistApiResponse;
+import lt.vcs.musicapp.model.ArtistsApiResponse;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
@@ -12,4 +13,7 @@ public interface UserDataService {
 
     @GET("?method=artist.getinfo")
     Call<ArtistApiResponse> getArtistInfoResults(@Query("artist") String artist_name, @Query("api_key") String api_key, @Query("format") String format);
+
+    @GET("?method=artist.search")
+    Call<ArtistsApiResponse> getArtistSearchResults(@Query("artist") String artist_name, @Query("api_key") String api_key, @Query("format") String format);
 }
--- app/src/main/java/lt/vcs/musicapp/repository/RemoteRepository.java
+++ app/src/main/java/lt/vcs/musicapp/repository/RemoteRepository.java
@@ -7,6 +7,7 @@ import static lt.vcs.musicapp.Constants.LOG_TAG;
 import android.util.Log;
 
 import lt.vcs.musicapp.model.ArtistApiResponse;
+import lt.vcs.musicapp.model.ArtistsApiResponse;
 import lt.vcs.musicapp.network.UserDataService;
 import lt.vcs.musicapp.network.UserServiceClient;
 import retrofit2.Call;
@@ -15,10 +16,9 @@ import retrofit2.Response;
 
 public class RemoteRepository {
 
-    public void getArtistInfoResults() {
+    UserDataService service = UserServiceClient.getUserInstance().create(UserDataService.class);
 
-        UserDataService service =
-                UserServiceClient.getUserInstance().create(UserDataService.class);
+    public void getArtistInfoResults() {
 
         Call<ArtistApiResponse> call = service.getArtistInfoResults("Cher", API_KEY, JSON_FORMAT);
 
@@ -38,5 +38,26 @@ public class RemoteRepository {
 
         call.enqueue(callback);
 
+    }
+
+    public void getArtistSearchResults() {
+
+        Call<ArtistsApiResponse> call = service.getArtistSearchResults("Cher", API_KEY, JSON_FORMAT);
+
+        Callback<ArtistsApiResponse> callback = new Callback<ArtistsApiResponse>() {
+
+            @Override
+            public void onResponse(Call<ArtistsApiResponse> call, Response<ArtistsApiResponse> response) {
+                Log.i(LOG_TAG, "onResponse: " + response.body());
+            }
+
+            @Override
+            public void onFailure(Call<ArtistsApiResponse> call, Throwable t) {
+                Log.i(LOG_TAG, "Failed to retrieve data: " + t.getMessage());
+                call.cancel();
+            }
+        };
+
+        call.enqueue(callback);
     }
 }
