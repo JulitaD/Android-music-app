e779ecc08d5ae89cc2e5107c69d6bb97e7e386a7
JulitaDargyte <julita.dargyte@bazaarvoice.com>
2022-01-30 16:12:35 +0200

    Add Biography.java

---
+++ app/src/main/java/lt/vcs/musicapp/model/Biography.java
@@ -0,0 +1,25 @@
+package lt.vcs.musicapp.model;
+
+public class Biography {
+
+    private String content;
+
+    public Biography(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "Bio{" +
+                "content='" + content + '\'' +
+                '}';
+    }
+}
