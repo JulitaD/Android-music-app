a9b219b803e697efd02e29760735f276d6996227
JulitaDargyte <julita.dargyte@bazaarvoice.com>
2022-02-02 17:30:30 +0200

    Add glide implementation

--- app/src/main/java/lt/vcs/musicapp/UI/recyclerview/ArtistsAdapter.java
+++ app/src/main/java/lt/vcs/musicapp/UI/recyclerview/ArtistsAdapter.java
@@ -4,10 +4,13 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.List;
 
@@ -35,6 +38,10 @@ public class ArtistsAdapter extends RecyclerView.Adapter<ArtistsAdapter.ViewHold
     @Override
     public void onBindViewHolder(@NonNull ArtistsAdapter.ViewHolder holder, int position) {
         holder.artistNameTextView.setText(artistList.get(position).getName());
+        Glide.with(context)
+                .load(artistList.get(position).getImage().get(3).getText())
+                .into(holder.imageView);
+
     }
 
     @Override
@@ -44,10 +51,12 @@ public class ArtistsAdapter extends RecyclerView.Adapter<ArtistsAdapter.ViewHold
 
     class ViewHolder extends RecyclerView.ViewHolder {
         TextView artistNameTextView;
+        ImageView imageView;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             artistNameTextView = itemView.findViewById(R.id.artistNameTextView);
+            imageView = itemView.findViewById(R.id.artistImageView);
         }
     }
 }
