f71d5e92878d8f191fa872117be330ac649b754f
JulitaDargyte <julita.dargyte@bazaarvoice.com>
2022-02-05 17:08:43 +0200

    Add artist, album and track button to MainActivity

--- app/src/main/java/lt/vcs/musicapp/MainActivity.java
+++ app/src/main/java/lt/vcs/musicapp/MainActivity.java
@@ -1,103 +1,131 @@
 package lt.vcs.musicapp;
 
-import static lt.vcs.musicapp.Constants.LOG_TAG;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-import java.util.Collections;
-import java.util.List;
-
-import lt.vcs.musicapp.main.album.viewModel.AlbumsAdapter;
-import lt.vcs.musicapp.main.artist.viewModel.ArtistsAdapter;
-import lt.vcs.musicapp.main.album.model.details.Album;
-import lt.vcs.musicapp.main.album.model.search.AlbumSearchResults;
-import lt.vcs.musicapp.main.artist.model.details.Artist;
-import lt.vcs.musicapp.main.artist.model.search.ArtistSearchResults;
-import lt.vcs.musicapp.main.album.model.details.TopAlbums;
-import lt.vcs.musicapp.main.track.model.search.TrackSearchResults;
+import lt.vcs.musicapp.main.album.model.search.AlbumSearchActivity;
+import lt.vcs.musicapp.main.artist.model.search.ArtistSearchActivity;
+import lt.vcs.musicapp.main.track.model.search.TrackSearchActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     MainActivityViewModel viewModel = null;
-    RecyclerView recyclerView;
-    ArtistsAdapter artistsAdapter;
-    AlbumsAdapter albumsAdapter;
-    List<Artist> artistList = Collections.emptyList();
-    List<Album> albumList = Collections.emptyList();
+    //    RecyclerView recyclerView;
+//    ArtistsAdapter artistsAdapter;
+//    AlbumsAdapter albumsAdapter;
+//    List<Artist> artistList = Collections.emptyList();
+//    List<Album> albumList = Collections.emptyList();
+    private Button artistsButton;
+    private Button albumsButton;
+    private Button tracksButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        artistsButton = findViewById(R.id.artistSearchButton);
+        albumsButton = findViewById(R.id.albumSearchButton);
+        tracksButton = findViewById(R.id.trackSearchButton);
+
+        setUpAlbumButton();
+        setUpArtistButton();
+        setUpTrackButton();
+
         viewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
 
 //        viewModel.fetchArtistInfo();
 //        viewModel.fetchArtistSearchResults();
 //        viewModel.fetchAlbumInfo();
-        viewModel.fetchAlbumSearchResults();
+//        viewModel.fetchAlbumSearchResults();
 //        viewModel.fetchTopAlbums();
 //        viewModel.fetchTrackSearchResults();
-        setUpObservers();
+//        setUpObservers();
 
-        recyclerView = findViewById(R.id.recycleView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
-        recyclerView.setAdapter(artistsAdapter);
+//        recyclerView = findViewById(R.id.recycleView);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+//        recyclerView.setAdapter(artistsAdapter);
 
     }
 
-    private void setUpObservers() {
+//    private void setUpObservers() {
+//
+//        viewModel.getArtist().observe(this, new Observer<Artist>() {
+//                    @Override
+//                    public void onChanged(Artist artist) {
+//                        Log.i(LOG_TAG, "Activity onResponse: " + artist);
+//                    }
+//                }
+//        );
+//        viewModel.getArtistSearchResults().observe(this, new Observer<ArtistSearchResults>() {
+//            @Override
+//            public void onChanged(ArtistSearchResults artistSearchResults) {
+//                Log.i(LOG_TAG, "Activity onResponse: " + artistSearchResults);
+//                artistsAdapter = new ArtistsAdapter(artistSearchResults.getArtistmatches().getArtist(), getApplication());
+//                recyclerView.setAdapter(artistsAdapter);
+//            }
+//        });
+//        viewModel.getAlbum().observe(this, new Observer<Album>() {
+//                    @Override
+//                    public void onChanged(Album album) {
+//                        Log.i(LOG_TAG, "Activity onResponse: " + album);
+//                    }
+//                }
+//        );
+//        viewModel.getAlbumSearchResults().observe(this, new Observer<AlbumSearchResults>() {
+//                    @Override
+//                    public void onChanged(AlbumSearchResults albumSearchResults) {
+//                        Log.i(LOG_TAG, "Activity onResponse: " + albumSearchResults);
+//                        albumsAdapter = new AlbumsAdapter(albumSearchResults.getAlbummatches().getAlbum(), getApplication());
+//                        recyclerView.setAdapter(albumsAdapter);
+//                    }
+//                }
+//        );
+//        viewModel.getTopAlbums().observe(this, new Observer<TopAlbums>() {
+//            @Override
+//            public void onChanged(TopAlbums topAlbums) {
+//                Log.i(LOG_TAG, "Activity onResponse: " + topAlbums);
+//            }
+//        });
+//        viewModel.getTrackResults().observe(this, new Observer<TrackSearchResults>() {
+//            @Override
+//            public void onChanged(TrackSearchResults trackSearchResults) {
+//                Log.i(LOG_TAG, "Activity onResponse: " + trackSearchResults);
+//            }
+//        });
+//
+//    }
 
-        viewModel.getArtist().observe(this, new Observer<Artist>() {
-                    @Override
-                    public void onChanged(Artist artist) {
-                        Log.i(LOG_TAG, "Activity onResponse: " + artist);
-                    }
-                }
-        );
-        viewModel.getArtistSearchResults().observe(this, new Observer<ArtistSearchResults>() {
+        private void setUpArtistButton() {
+        artistsButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(ArtistSearchResults artistSearchResults) {
-                Log.i(LOG_TAG, "Activity onResponse: " + artistSearchResults);
-                artistsAdapter = new ArtistsAdapter(artistSearchResults.getArtistmatches().getArtist(), getApplication());
-                recyclerView.setAdapter(artistsAdapter);
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ArtistSearchActivity.class);
+                startActivity(intent);
             }
         });
-        viewModel.getAlbum().observe(this, new Observer<Album>() {
-                    @Override
-                    public void onChanged(Album album) {
-                        Log.i(LOG_TAG, "Activity onResponse: " + album);
-                    }
-                }
-        );
-        viewModel.getAlbumSearchResults().observe(this, new Observer<AlbumSearchResults>() {
-                    @Override
-                    public void onChanged(AlbumSearchResults albumSearchResults) {
-                        Log.i(LOG_TAG, "Activity onResponse: " + albumSearchResults);
-                        albumsAdapter = new AlbumsAdapter(albumSearchResults.getAlbummatches().getAlbum(), getApplication());
-                        recyclerView.setAdapter(albumsAdapter);
-                    }
-                }
-        );
-        viewModel.getTopAlbums().observe(this, new Observer<TopAlbums>() {
+    }
+    private void setUpAlbumButton() {
+        albumsButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(TopAlbums topAlbums) {
-                Log.i(LOG_TAG, "Activity onResponse: " + topAlbums);
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AlbumSearchActivity.class);
+                startActivity(intent);
             }
         });
-        viewModel.getTrackResults().observe(this, new Observer<TrackSearchResults>() {
+    }
+    private void setUpTrackButton() {
+        tracksButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(TrackSearchResults trackSearchResults) {
-                Log.i(LOG_TAG, "Activity onResponse: " + trackSearchResults);
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TrackSearchActivity.class);
+                startActivity(intent);
             }
         });
-
     }
 }
